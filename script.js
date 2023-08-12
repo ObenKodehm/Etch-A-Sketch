a28d658e8c8bbccf35bf7212f44802b0bb166965
Ashutosh Taneja <obenkodehm@gmail.com>
2023-08-13 00:40:05 +0530

    Enhance readability and clarity of code

--- script.js
+++ script.js
@@ -1,97 +1,119 @@
-const sketch = (function sketchGrid() {
+// Initialize the sketch object using an immediately invoked function expression (IIFE).
+const sketch = (function createSketch() {
   const sketchContainer = document.getElementById('sketch-container');
 
-  function setGrid(n) {
+  // Set the grid template rows and columns to create a grid layout.
+  function setGridSize(n) {
     sketchContainer.style.gridTemplateRows = `repeat(${n}, minmax(0, 1fr))`;
     sketchContainer.style.gridTemplateColumns = `repeat(${n}, minmax(0, 1fr))`;
   }
 
-  function clearGrid() {
-    const gridSize = sketchContainer.childElementCount;
-    const gridCollection = sketchContainer.children;
-    for (let i = 0; i < gridSize; i += 1) {
-      const gridElement = gridCollection.item(i);
-      gridElement.style.backgroundColor = '#FCF6F5FF';
-      gridElement.classList.toggle('border', true);
+  // Clear the grid by resetting background colors and applying border to grid elements.
+  function clearSketch() {
+    const totalGridElements = sketchContainer.childElementCount;
+    const gridElementCollection = sketchContainer.children;
+    for (let i = 0; i < totalGridElements; i += 1) {
+      const gridElement = gridElementCollection.item(i);
+      gridElement.style.backgroundColor = '#FCF6F5FF'; // Reset background color
+      gridElement.classList.toggle('border', true); // Apply border class
     }
   }
 
-  function createDiv() {
-    const divElement = document.createElement('div');
-    divElement.classList = 'border border-solid border-[#BFBFBF] w-auto h-auto';
-    return divElement;
+  // Create a new div element with specified CSS classes.
+  function createGridElement() {
+    const gridElement = document.createElement('div');
+    gridElement.classList = 'border border-solid border-[#BFBFBF] w-auto h-auto';
+    return gridElement;
   }
 
-  function createAndModifyGrid(n) {
-    const m = (n * n) - sketchContainer.childElementCount;
+  // Create and modify the grid based on the specified size.
+  function adjustGridSize(newSize) {
+    // calculate difference between number of elements present in grid and number of elements needed
+    const diff = (newSize * newSize) - sketchContainer.childElementCount;
 
-    setGrid(n);
+    setGridSize(newSize); // update grid layout
 
-    if (m > 0) {
-      for (let i = 0; i < m; i += 1) {
-        const gridBlock = createDiv();
-        sketchContainer.appendChild(gridBlock);
+    if (diff > 0) {
+      // Add new grid blocks if required
+      for (let i = 0; i < diff; i += 1) {
+        const gridElement = createGridElement();
+        sketchContainer.appendChild(gridElement);
       }
     } else {
-      for (let i = 0; i < Math.abs(m); i += 1) {
+      // Remove excess grid blocks if required
+      for (let i = 0; i < Math.abs(diff); i += 1) {
         sketchContainer.removeChild(sketchContainer.lastElementChild);
       }
     }
   }
 
-  return { createAndModifyGrid, clearGrid };
+  // return public methods for the sketch objects
+  return { adjustGridSize, clearSketch };
 }());
 
-(function setButtons() {
-  const sizeButton = document.querySelector('button[name="Grid Size"]');
-  sizeButton.addEventListener('click', () => {
-    const gridSize = prompt('Enter a grid size between 1 and 64: ');
+// Define event listeners and handlers for UI buttons and interactions.
+(function setupUIInteractions() {
+  // Event listener for grid size button.
+  const gridSizeButton = document.querySelector('button[name="Grid Size"]');
+  gridSizeButton.addEventListener('click', () => {
+    // change grid size by prompting the user
+    const newSize = prompt('Enter a grid size between 1 and 64: ');
 
-    if (Number.isNaN(gridSize) === NaN || gridSize < 1 || gridSize > 64) {
+    if (Number.isNaN(newSize) === NaN || newSize < 1 || newSize > 64) {
       alert('Enter a valid grid size between 1 and 64.');
     } else {
-      sketch.clearGrid();
-      sketch.createAndModifyGrid(Number(gridSize));
+      sketch.clearSketch();
+      sketch.adjustGridSize(Number(newSize));
     }
   });
 
+  // Event listener for color choice button.
   const colorChoiceButton = document.querySelector('button[name="Choose Color"]');
-  const colorChoices = document.getElementById('color-choice');
+  const colorChoicesContainer = document.getElementById('color-choice');
   colorChoiceButton.addEventListener('click', () => {
-    colorChoices.classList.toggle('hidden');
-    colorChoices.classList.toggle('flex');
+    // toggle the tailwind css class to hide the color choices
+    colorChoicesContainer.classList.toggle('hidden');
+    colorChoicesContainer.classList.toggle('flex');
   });
 
+  // Event listener for clear button.
   const clearButton = document.querySelector('button[name="Clear"]');
   clearButton.addEventListener('click', () => {
-    sketch.clearGrid();
+    // clear the grid
+    sketch.clearSketch();
   });
 }());
 
-let gridColor = () => '#000';
+// Define the initial grid color and set up event listener for mouseover interactions.
+let currentGridColor = () => '#000'; // Default grid Color
 const sketchContainer = document.getElementById('sketch-container');
 sketchContainer.addEventListener('mouseover', (event) => {
+  // use event delegation to set the bakcground color and remove border class
   const gridElement = event.target;
   if (gridElement.id !== 'sketch-container') {
-    gridElement.style.backgroundColor = gridColor();
+    gridElement.style.backgroundColor = currentGridColor();
     gridElement.classList.toggle('border', false);
   }
 });
 
-(function setColorButtons() {
-  function showColorChoice(choice) {
-    const currentColorPara = document.getElementById('color');
-    currentColorPara.textContent = choice;
+// Define color choice button handlers and functions for changing grid colors.
+(function setupColorChoiceButtons() {
+  // Update the displayed current color choice.
+  function displaySelectedColor(choice) {
+    const selectedColorPara = document.getElementById('color');
+    selectedColorPara.textContent = choice;
   }
 
+  // Event listener for black color button.
   const blackColorButton = document.querySelector('button[name="Black"]');
   blackColorButton.addEventListener('click', () => {
-    gridColor = () => '#000';
-    sketch.clearGrid();
-    showColorChoice('Color: #000');
+    currentGridColor = () => '#000'; // Set grid color to black
+    sketch.clearSketch();
+    displaySelectedColor('Color: #000');
   });
 
-  const getRandomColor = () => {
+  // Generate a random color.
+  const generateRandomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
@@ -99,28 +121,33 @@ sketchContainer.addEventListener('mouseover', (event) => {
     return `rgb(${r}, ${g}, ${b})`;
   };
 
+  // Event listener for randomize color button.
   const randomizeColorButton = document.querySelector('button[name="Randomize"]');
   randomizeColorButton.addEventListener('click', () => {
-    const randomColor = getRandomColor();
-    gridColor = () => randomColor;
-    sketch.clearGrid();
-    showColorChoice(`Color: ${randomColor}`);
+    const randomColor = generateRandomColor();
+    currentGridColor = () => randomColor; // Set current grid color to a random color
+    sketch.clearSketch();
+    displaySelectedColor(`Color: ${randomColor}`);
   });
 
+  // Event listener for chaos color button.
   const chaosColorButton = document.querySelector('button[name="Chaos"]');
   chaosColorButton.addEventListener('click', () => {
-    gridColor = () => getRandomColor();
-    sketch.clearGrid();
-    showColorChoice('Just A Chaos.');
+    // On every mouseover set current grid color to a random color
+    currentGridColor = () => generateRandomColor();
+    sketch.clearSketch();
+    displaySelectedColor('Just A Chaos.');
   });
 
+  // Event listener for color picker.
   const colorPicker = document.getElementById('color-picker');
   colorPicker.addEventListener('change', (event) => {
-    const colorPick = event.target.value;
-    gridColor = () => colorPick;
-    sketch.clearGrid();
-    showColorChoice(`Color: ${colorPick}`);
+    const pickedColor = event.target.value;
+    currentGridColor = () => pickedColor; // Set current grid color to the selected color
+    sketch.clearSketch();
+    displaySelectedColor(`Color: ${pickedColor}`);
   });
 }());
 
-sketch.createAndModifyGrid(16);
+// Initialize the sketch grid with a default size of 16.
+sketch.adjustGridSize(16);
